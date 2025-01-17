fa19505235d1f8be834322484971f60c47e08f2c
Binayak Basu <basubinayak05@hotmail.com>
2025-01-17 23:01:22 +0530

    new commit

--- Linked List/SinglyLinkedList.py
+++ Linked List/SinglyLinkedList.py
@@ -19,6 +19,15 @@ class SinglyLinkedList:
             self.head = new_node
             self.tail = new_node
             self.length = 1
+
+    def __repr__(self):
+        """Provides a string representation of the linked list."""
+        nodes = []
+        current = self.head
+        while current: # Traverse the Linked List 
+            nodes.append(str(current.value)) # Add the data to the list
+            current = current.next
+        return " -> ".join(nodes) if nodes else "Empty Linked List"
     
     def append(self, value):
         """
@@ -34,14 +43,16 @@ class SinglyLinkedList:
             self.tail = new_node  # Move the tail to the new node
         self.length+=1
     
-    def __repr__(self):
-        """Provides a string representation of the linked list."""
-        nodes = []
-        current = self.head
-        while current: # Traverse the Linked List 
-            nodes.append(str(current.value)) # Add the data to the list
-            current = current.next
-        return " -> ".join(nodes) if nodes else "Empty Linked List"
+    def prepend(self, value):
+        """Inserts a new node with the given value at the beginning of the list."""
+        new_node = Node(value)  # Create a new node
+        if self.head is None:  # Edge case: list is empty
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head  # Point the new node to the current head
+            self.head = new_node  # Update the head to the new node
+    
     
             
 
@@ -50,17 +61,17 @@ if __name__=="__main__":
     node = Node(20)
     print(node) # [20|None]
     
-    # Create an empty linked list
+    ###### Create an empty linked list #######
     list1 = SinglyLinkedList()
     print(list1.head)  # Output: None
     print(list1.tail)  # Output: None
 
-    # Create a linked list with an initial node
+    ###### Create a linked list with an initial node ######
     list2 = SinglyLinkedList(10)
     print(list2.head.value)  # Output: 10
     print(list2.tail.value)  # Output: 10
 
-    # Creating an empty linked list
+    ###### Creating an empty linked list and append node ######
     sll = SinglyLinkedList()
     sll.append(5)  # Appending to an empty list
     print(sll.head.value)  # Output: 5
@@ -70,6 +81,17 @@ if __name__=="__main__":
     print(sll.head.value)  # Output: 5
     print(sll.head.next.value)  # Output: 10
     print(sll.tail.value)  # Output: 10
+    
+    ###### Prepend node ######
+    sll = SinglyLinkedList()
+    print(sll)  # Output: Empty Linked List
 
-    # View the Linked List
-    print(sll)
+    # Insert at the beginning
+    sll.prepend(10)
+    print(sll)  # Output: 10
+
+    sll.prepend(20)
+    print(sll)  # Output: 20 -> 10
+
+    sll.prepend(30)
+    print(sll)  # Output: 30 -> 20 -> 10
