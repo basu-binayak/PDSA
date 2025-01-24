4072f9109d82dc6755143e65f7bf12af2ae10560
Binayak Basu <basubinayak05@hotmail.com>
2025-01-25 00:13:52 +0530

    new commit

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -28,6 +28,39 @@ class SelectionSort:
             self.lst[i], self.lst[min_index] = self.lst[min_index], self.lst[i]
             
         return self.lst
+    
+    def stable_sort(self):
+        """
+        Performs a stable version of selection sort on the list.
+        """
+        # get the length of the list provided 
+        length = len(self.lst)
+        
+        # iterate over the list
+        for i in range(length-1):
+            # i - in each iteration represents the first element of the unsorted array 
+            
+            min_index = i # consider the index of the first element of the unsorted array as the index for the minimum element
+            
+            # iterate over the array to the right of the first element of the unsorted array 
+            for j in range(i+1,length):
+                # find the minimum element 
+                if(self.lst[j]<self.lst[min_index]):
+                    min_index=j
+                    
+            # Instaed od swapping directly, we will store the index for the minimum value in a variable say, key
+            key = self.lst[min_index]
+            
+            # The aim is to shift the elements between the current index , i and the min_index one position to the right and fill the i-th position with the key value
+            while min_index  > i:
+                self.lst[min_index] = self.lst[min_index-1]
+                min_index-=1
+            
+            self.lst[i] = key
+    
+        return self.lst
+        
+
 
 if __name__=="__main__":
     input_array = [64, 25, 12, 22, 11]
@@ -37,4 +70,9 @@ if __name__=="__main__":
     print("Input array:", input_array)
     print("Sorted array:", sorted_array) 
                 
-            
+    input_array = [(3, 'A'), (3, 'B'), (1, 'C')]
+    sorter = SelectionSort(input_array)
+    sorted_array = sorter.stable_sort()
+
+    print("Input array:", input_array)
+    print("Sorted array:", sorted_array)        
